f116f8b1171b7e42f1d64effd67d896d83d3a0c0
Jarrod Williams <jarroddw98@gmail.com>
2018-04-11 22:00:56 +1000

    Implemented solution to Q4

--- MATLAB files/assignment.m
+++ MATLAB files/assignment.m
@@ -94,9 +94,62 @@ end
 
 
 
+
+
+
+
+
 %% 4. Nearest Neighbor Classifiers
 
+% Import the dataset.
+iris = load('data/iris.txt');
+pi = randperm(size(iris, 1));
+Y= iris(pi, 5); X = iris(pi, 1:2);
 
+% (A) - Plot dataset by feature values.
+figure('Name','Feature Values of Iris Dataset');
+hold on;
+colours = unique(Y);
+for colour = 1:length(colours)
+    scatterColour = [0,0,0];
+    scatterColour(colour) = 1;
+    featureIndicies = find(Y==colours(colour));
+    points = X(featureIndicies, 1:end);
+    pointsX = points(1:end, 1);
+    pointsY = points(1:end, 2);
+    scatter(pointsX, pointsY, [], scatterColour, 'filled');
+end
+hold off;
+
+% (B) Learn and plot 1-nearest-neighbour predictor
+learner = knnClassify(1, X, Y);
+class2DPlot(learner,X,Y);
+
+% (C) Repeat for several values of k
+kValues = [3,10,30]; % k=1 is already plotted above in (B).
+for index = 1:length(kValues)
+    learner = knnClassify(kValues(index), X, Y);
+    class2DPlot(learner,X,Y);
+end
+
+% (D) Split data into training (80%) and valuation (20%) data. Train and 
+%     validate model for multiple values of k and calculate its
+%     performance.
+Xtrain = X(1:118, 1:end);
+Xvalid = X(119:end, 1:end);
+Ytrain = Y(1:118);
+Yvalid = Y(119:end);
+kValues = [1, 2, 5, 10, 50, 100, 200];
+errors = [];
+for index = 1:length(kValues)
+    learner = knnClassify(kValues(index), Xtrain, Ytrain); % train model on X/Ytrain
+    Yhat = predict(learner, Xvalid); % predict results on Xtrain/Yvalid
+    errors = [errors, numel(find(Yhat~=Yvalid))]; % count what fraction of predictions are wrong   
+    hold on;
+end
+
+figure('Name','Performace of k'); % Plot performance of k against error
+plot(kValues, errors,'-x')
 
 
 % 5. Perceptrons and Logistic Regression
